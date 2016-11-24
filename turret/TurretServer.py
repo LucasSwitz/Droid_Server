392199d23a7adb2c4c58ce5993ebc75086373f0e
Lucas Switzer <lucswitz@gmail.com>
2016-11-24 01:27:58 -0500

    String normalization

--- turret/TurretServer.py
+++ turret/TurretServer.py
@@ -18,8 +18,19 @@ class TurretServer(Server):
 
     def parse_cli(self, data):
         cli = CLInterpreter()
-        (base_command,args) = self.get_cli_command(data)
+        data = self.normalize_string(data)
+        (base_command, args) = self.get_cli_command(data)
         cli.run_cli_command(base_command, args)
+
+    @staticmethod
+    def normalize_string(string):
+        while '  ' in string:
+            string = str.replace(string, '  ', ' ')
+        if string[len(string)-1] == ' ':
+            string = string[0:len(string) - 1]
+        if string[0] == ' ':
+            string = string[1:len(string)]
+        return string
 
     def get_cli_command(self, user_input):
         base_command = ""
